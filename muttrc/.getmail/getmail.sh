4d64f2ce33d7c38e60f25e73061f3117dd67339a
Wanlong Gao <wanlong.gao@gmail.com>
2015-09-23 09:44:42 +0800

    mutt: update mutt configs
    Signed-off-by: Wanlong Gao <wanlong.gao@gmail.com>

--- muttrc/.getmail/getmail.sh
+++ muttrc/.getmail/getmail.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 export PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin
 #setlock -n ~/.mutt/getmail_gmail.lock getmail -n -r ~/.getmail/getmailrc.gmail >>~/.mutt/getmaillog 2>&1
-setlock -n ~/.mutt/getmail_allen_gmail.lock getmail -n -r ~/.getmail/getmailrc.allen_gmail >>~/.mutt/getmaillog 2>&1
-setlock -n ~/.mutt/getmail_allen_kernel.lock getmail -n -r ~/.getmail/getmailrc.allen_kernel >>~/.mutt/getmaillog 2>&1
+#setlock -n ~/.mutt/getmail_allen_gmail.lock getmail -n -r ~/.getmail/getmailrc.allen_gmail >>~/.mutt/getmaillog 2>&1 &
+setlock -n ~/.mutt/getmail_allen_kernel.lock getmail -n -r ~/.getmail/getmailrc.allen_kernel >>~/.mutt/getmaillog 2>&1 &
+setlock -n ~/.mutt/getmail_easystack.lock getmail -n -r ~/.getmail/getmailrc.easystack >>~/.mutt/getmaillog 2>&1 &
