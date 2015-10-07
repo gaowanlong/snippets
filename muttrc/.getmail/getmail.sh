40f369e8a069c36026ffb04093854350fe93a116
Wanlong Gao <wanlong.gao@gmail.com>
2015-10-07 09:58:13 +0800

    mutt: use outlook for subscribe
    gmail->163.com, 163 will reject the delivery mails from gmail, WTF.
    just switch to outlook.com for subscribe vger.kernel.org.
    
    Signed-off-by: Wanlong Gao <wanlong.gao@gmail.com>

--- muttrc/.getmail/getmail.sh
+++ muttrc/.getmail/getmail.sh
@@ -2,5 +2,6 @@
 export PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin
 #setlock -n ~/.mutt/getmail_gmail.lock getmail -n -r ~/.getmail/getmailrc.gmail >>~/.mutt/getmaillog 2>&1
 #setlock -n ~/.mutt/getmail_allen_gmail.lock getmail -n -r ~/.getmail/getmailrc.allen_gmail >>~/.mutt/getmaillog 2>&1 &
-setlock -n ~/.mutt/getmail_allen_kernel.lock getmail -n -r ~/.getmail/getmailrc.allen_kernel >>~/.mutt/getmaillog 2>&1 &
 setlock -n ~/.mutt/getmail_easystack.lock getmail -n -r ~/.getmail/getmailrc.easystack >>~/.mutt/getmaillog 2>&1 &
+setlock -n ~/.mutt/getmail_allen_kernel.lock getmail -n -r ~/.getmail/getmailrc.allen_kernel >>~/.mutt/getmaillog 2>&1 &
+setlock -n ~/.mutt/getmail_outlook.lock getmail -n -r ~/.getmail/getmailrc.outlook >>~/.mutt/getmaillog 2>&1 &
