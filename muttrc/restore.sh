1fca71adde2db4374910c02f14b814a02d1eac5a
Wanlong Gao <wanlong.gao@gmail.com>
2015-10-07 11:05:34 +0800

    mutt: update restore script
    copy security configs from remore host.
    
    Signed-off-by: Wanlong Gao <wanlong.gao@gmail.com>

--- muttrc/restore.sh
+++ muttrc/restore.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-[ "$PWD" = "/git/snippets/muttrc" ] || {
+[[ "$PWD" =~ "git/snippets/muttrc" ]] || {
 	echo "WARN: avoid removing wrong directory!"
 	exit 1
 }
@@ -11,3 +11,9 @@ cp -av .muttrc ~/.muttrc
 mkdir -p ~/.mutt-rc
 cp -av .mutt-rc/*.muttrc ~/.mutt-rc/
 cp -av .mailcap ~/.mailcap
+
+cp -av .getmail/getmail.sh ~/.getmail/getmail.sh
+rm -f ~/.getmail/getmailrc.*
+scp big:~/.getmail/getmailrc.* ~/.getmail/
+
+scp big:~/.msmtprc ~/.msmtprc
